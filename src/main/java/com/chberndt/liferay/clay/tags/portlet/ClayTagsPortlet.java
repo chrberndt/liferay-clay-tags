f1f8da75b9069aa10a82a5c95f9b145adadf4f88
Christian Berndt <christian.berndt@liferay.com>
2020-02-19 23:47:04 +0100

    Configure portlet specific css.

--- src/main/java/com/chberndt/liferay/clay/tags/portlet/ClayTagsPortlet.java
+++ src/main/java/com/chberndt/liferay/clay/tags/portlet/ClayTagsPortlet.java
@@ -17,6 +17,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.css-class-wrapper=liferay-clay-tags",
 		"javax.portlet.display-name=ClayTags",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
